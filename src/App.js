670cd772034a0d17d39f240886e50e57b413ef8c
Harveen Atwal <harveenatwalbusiness@gmail.com>
2022-10-02 00:23:08 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="App-portrait"></div>
+        <div className="App-rsvp text_center">
+          <h1 className="App-headline">Gurpreet and Harveen</h1>
+          <h5 className="App-subhead">October 14, 2023</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
