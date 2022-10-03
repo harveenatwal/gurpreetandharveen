fdb133e7b97fe63d551343de70079b49e3a02a39
Harveen Atwal <harveenatwalbusiness@gmail.com>
2022-10-02 20:28:11 -0700

    Different fades for each section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import gurpreetandharveen from "./gurpreetandharveen.png";
 
 function App() {
   return (
     <div className="App">
-      {/* <section className="App-section App-portrait"></section> */}
-      <section className="App-section text_center relative">
+      <section className="App-section text_center relative z-9">
+        <div className="App-section-inner sticky">
+          <img className="App-portrait" src={gurpreetandharveen} />
+        </div>
+        <div className="App-fade-out"></div>
+      </section>
+      <section className="App-section text_center relative z-8">
         <div className="App-section-inner sticky">
           <h1 className="App-headline">Gurpreet and Harveen</h1>
           <div className="mt-3 text-secondary">
@@ -12,6 +18,7 @@ function App() {
             <h6 className="App-footnote mt-1">October 14, 2023</h6>
           </div>
         </div>
+        <div className="App-fade-in"></div>
         <div className="App-fade-out"></div>
       </section>
       <section className="App-section text_center relative">
@@ -19,7 +26,6 @@ function App() {
           <h1 className="App-headline">Rsvp</h1>
         </div>
         <div className="App-fade-in"></div>
-        {/* <div className="App-fade-out"></div> */}
       </section>
     </div>
   );
