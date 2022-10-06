633917970667dadcf4af647c477dc90ac3ccc7c4
Harveen Atwal <harveenatwalbusiness@gmail.com>
2022-10-05 19:54:01 -0700

    Add save the date details + rsvp

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ function App() {
       <section className="App-section text_center relative z-9">
         <div className="App-section-inner sticky">
           <img className="App-portrait" src={gurpreetandharveen} />
-          <div className="mt-3 text-secondary">
-            <h1 className="App-headline">Gurpreet and Harveen</h1>
-            <h5 className="App-footnote">Fremont, California</h5>
-            <h6 className="App-footnote mt-1">October 14, 2023</h6>
+          <div className="mt-3">
+            <h1 className="h2 autography">Save the date</h1>
+            <h6 className="h6 caps text-tertiary mt-3">For the marriage of</h6>
+            <h2 className="h4 caps flex items-center justify-center mt-1">
+              Gurpreet <span className="h6 mx-2 text-secondary">•</span> Harveen
+            </h2>
+            <div className="flex justify-center items-center mt-2">
+              <h6 className="h6 caps text-secondary">Oct</h6>
+              <div className="App-date-divider"></div>
+              <h4 className="h4 caps">14</h4>
+              <div className="App-date-divider"></div>
+              <h6 className="h6 caps text-secondary">2023</h6>
+            </div>
+            <h6 className="h6 mt-1 caps text-secondary">Fremont, Ca</h6>
           </div>
           <div style={{ display: "flex", height: "50px", width: "1px" }}></div>
         </div>
@@ -18,7 +28,7 @@ function App() {
       </section>
       {/* <section className="App-section text_center relative z-8">
         <div className="App-section-inner sticky">
-          <h1 className="App-headline">Gurpreet and Harveen</h1>
+          <h1 className="autography">Gurpreet and Harveen</h1>
           <div className="mt-3 text-secondary">
             <h5 className="App-footnote">Fremont, California</h5>
             <h6 className="App-footnote mt-1">October 14, 2023</h6>
@@ -29,7 +39,16 @@ function App() {
       </section> */}
       <section className="App-section text_center relative">
         <div className="App-section-inner sticky">
-          <h1 className="App-headline">Rsvp</h1>
+          <a
+            className="flex flex-col App-rsvp-link"
+            href="https://forms.gle/5cdnLXzhqjQjVXLX8"
+          >
+            <h6 className="h6 text-tertiary">Tap to</h6>
+            <h1 className="h1 autography mt-4">Rsvp</h1>
+          </a>
+          <h6 className="h5 mt-1 text-tertiary autography italic absolute App-invitation-follow">
+            formal invitation to follow
+          </h6>
         </div>
         <div className="App-fade-in"></div>
       </section>
